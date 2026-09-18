22c7554cce3c1b9046ba9ed20dd8da8aea14b60a
Noir <you@example.com>
2026-09-18 19:40:20 +0700

    Add Galaxy A23 4G

--- target/a23/sff.sh
+++ target/a23/sff.sh
@@ -1,7 +1,10 @@
 # Copyright (c) 2026 Salvo Giangreco
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# SEC Floating Feature configuration file for Galaxy A52 (a52q)
+# SEC Floating Feature configuration file for Galaxy A05s (a05s)
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
 
 # Enable extra brightness feature
 SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
